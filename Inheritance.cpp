cd7ac6d7d9b3a67647a937960e348edad3f87b87
jacobbria <jacob.bria@sinclair.edu>
2023-07-25 16:49:15 -0400

    Main() is complete with successful testing of input/output

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -13,15 +13,24 @@ using namespace std;
 // prototypes
 Vehicle getVehicle();
 void displayVehicle(Vehicle);
+Car getCar();
+void displayCar(Car);
+Truck getTruck();
+void displayTruck(Truck);
 
 
 int main()
 {
     Vehicle vehicle = getVehicle(); // get Vehicle information and store in vehicle object
 
-    displayVehicle(vehicle);
+    displayVehicle(vehicle);    // display the input vehicle information
 
-    
+    Car car = getCar();
+
+    displayCar(car);
+
+    Truck truck = getTruck();
+
 }
 
 Vehicle getVehicle() {  // get vehicle information for  user
@@ -35,6 +44,8 @@ Vehicle getVehicle() {  // get vehicle information for  user
 
     cout << "Enter year Vehicle was built: ";
     cin >> inputYearBuilt;
+    
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // debugging issue with incorrect user input (update: it worked)
 
     Vehicle vehicle(inputManufactor, inputYearBuilt);
     return vehicle;
@@ -44,5 +55,58 @@ Vehicle getVehicle() {  // get vehicle information for  user
 void displayVehicle(Vehicle vehicle) {  // display the vehicle information
 
     vehicle.displayInfo();  // call the function from vehicle object aka parent class
+
+}
+
+Car getCar() {
+    
+    int doors;
+    string inputManufactor;
+    int inputYearBuilt;
+
+
+    cout << "Enter Vehicle's manufacturer: ";
+    getline(cin, inputManufactor);
+
+    cout << "Enter year Vehicle was built: ";
+    cin >> inputYearBuilt;
+
+    cout << "Number of doors on car: ";
+    cin >> doors;
+
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // eat the floating newline
+
+
+    Car car(doors, inputManufactor, inputYearBuilt);    // instantiate a new car to store the input
+    return car; // return the input car obj to main
+}
+
+void displayCar(Car car) {
+
+    car.displayInfo();
+
+}
+
+Truck getTruck() {
+    double towCapacity;
+    string inputManufactor;
+    int inputYearBuilt;
+
+
+    cout << "Enter Vehicle's manufacturer: ";
+    getline(cin, inputManufactor);
+
+    cout << "Enter year Vehicle was built: ";
+    cin >> inputYearBuilt;
+
+    cout << "Enter towing capacity of truck in lb: ";
+    cin >> towCapacity;
+
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // eat newline
+
+
+    Truck truck(towCapacity, inputManufactor, inputYearBuilt);    // instantiate a new truck to store the input
+    return truck; // return the input car obj to main
+
 
 }
