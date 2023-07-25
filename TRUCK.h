34327c9c1fa1104a0387d9dbb8bf81c745d1d9ed
jacobbria <jacob.bria@sinclair.edu>
2023-07-25 16:20:50 -0400

    Created Truck Class

---
+++ TRUCK.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef TRUCK.h
+#define TRUCK.h
+#include "Vehicle.h"
+#include <string>
+#include <iostream>
+
+class Truck : public Vehicle {
+
+private:
+	double towingCapacity;
+public: 
+	Truck() : Vehicle() {	// default constructor
+		towingCapacity = 0.0;
+	}
+	
+	Truck(double tC, const string& m, int yB) : Vehicle(m, yB) {	// input constructor
+		towingCapacity = tC;
+
+	}
+
+	void setTruck(double tC) {
+		towingCapacity = tC;
+	}
+
+	double getTruck() {
+		return towingCapacity;
+	}
+
+	void displayInfo() const override {
+		cout << "Manufacturer: " << manufacturer << "." << endl;
+		cout << "Built: " << yearBuilt << ". " << endl;
+		cout << "Towing capacity: " << towingCapacity << ". " << endl;
+
+	}
+
+};
+#endif
